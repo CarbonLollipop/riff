3f57c0b4bba7a17fd8e7cc6fc36883938bfdb3b2
CarbonLollipop <georgiy.rha@gmail.com>
2023-05-12 16:06:47 -0700

    Cleaning Part 1

--- main.c
+++ main.c
@@ -9,10 +9,11 @@
 #include <SDL2/SDL_mixer.h>
 
 void quit() {
-    printf("Bye!");
     
     endwin();
-    
+   
+    printf("Bye!\n");
+
     Mix_CloseAudio();
     SDL_Quit();
     
@@ -20,45 +21,11 @@ void quit() {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("Usage: %s <song/directory of songs>\n", argv[0]);
         return 1;
     }
-
-    signal(SIGINT, quit);
-
-    // Do I like this? No. Does it work?
-
-    struct stat sb;
-
-    int playlist = 0;
-    char* songs[256];
-
-    if (stat(argv[1], &sb) == 0 && S_ISDIR(sb.st_mode)) {
-        playlist = 1;
-
-        int numSongs = 0;
-
-        struct dirent* file;
-
-        DIR* directory = opendir(argv[1]);
-
-        while ((file = readdir(directory)) != NULL && numSongs < 256) {
-            const char* extension = strrchr(file->d_name, '.');
-            if (extension != NULL && (strcmp(extension, ".mp3") == 0 || strcmp(extension, ".wav") == 0 || strcmp(extension, ".flac") == 0)) {
-                songs[numSongs] = (char*) malloc(256 * sizeof(char));
-                strncpy(songs[numSongs], file->d_name, 255);
-                songs[numSongs][255] = '\0';
-
-                numSongs++;
-            }
-        }
-
-        closedir(directory);
-    } else {
-        songs[0] = argv[1];
-    }
-
+    
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         printf("Failed to initialize SDL audio system: %s\n", SDL_GetError());
         return 1;
@@ -68,6 +35,35 @@ int main(int argc, char* argv[]) {
         printf("Failed to open the audio device: %s\n", Mix_GetError());
         return 1;
     }
+   
+    signal(SIGINT, quit);
+
+    struct stat sb;
+
+    char* queue[256];
+
+    if (stat(argv[1], &sb) == 0 && S_ISDIR(sb.st_mode)) {
+        int numSongs = 0;
+
+        struct dirent* file;
+
+        DIR* directory = opendir(argv[1]);
+
+        while ((file = readdir(directory)) != NULL && numSongs < 256) {
+            const char* extension = strrchr(file->d_name, '.');
+            if (extension != NULL && (strcmp(extension, ".mp3") == 0 || strcmp(extension, ".wav") == 0 || strcmp(extension, ".flac") == 0)) {
+                queue[numSongs] = (char*) malloc(256 * sizeof(char));
+                strncpy(queue[numSongs], file->d_name, 255);
+                queue[numSongs][255] = '\0';
+
+                numSongs++;
+            }
+        }
+
+        closedir(directory);
+    } else if (stat(argv[1], &sb) == 0 && S_ISREG(sb.st_mode)) {
+        queue[0] = argv[1];
+    }
 
     initscr();
     nodelay(stdscr, TRUE);
@@ -75,10 +71,10 @@ int main(int argc, char* argv[]) {
   
     printw("(P)ause/(P)lay\n");
     printw("(S)kip\n");
-    printw("(Q)uit\n\n");
+    printw("(Q)uit\n-------\n");
 
-    for(int i = 0; i < sizeof songs / sizeof songs[0]; i++) {
-        Mix_Music* music = Mix_LoadMUS(songs[i]);
+    for(int i = 0; i < sizeof queue / sizeof queue[0]; i++) {
+        Mix_Music* music = Mix_LoadMUS(queue[i]);
         
         if (!music) {
             printf("%s", Mix_GetError());
@@ -87,7 +83,7 @@ int main(int argc, char* argv[]) {
 
         Mix_PlayMusic(music, 0);
 
-        printw("Now playing %s\n", songs[i]);
+        printw("Now playing %s\n", queue[i]);
 
         while (Mix_PlayingMusic()) {
             SDL_Delay(1);
