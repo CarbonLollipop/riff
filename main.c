06501c5f0f46736f1279acef207ec6a7a9497a81
CarbonLollipop <carbonlollipop@gmail.com>
2023-05-11 23:14:45 -0700

    ALMOST works. Very buggy. Will fix tomorrow.

--- main.c
+++ main.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
 #include <signal.h>
+#include <ncurses.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
-#include <ncurses.h>
 
 void sigint_handler(int sig) {
-    // TODO Hide that pesky little ^C
-
-    printf("\nBye!\n");
+    printf("Bye!");
     exit(0);
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <song>\n", argv[0]);
+        printf("Usage: %s <song/directory of songs>\n", argv[0]);
         return 1;
     }
 
     signal(SIGINT, sigint_handler);
+
+    // Do I like this? No. Does it work?
+
+    struct stat sb;
+
+    int playlist = 0;
+    char* songs[256];
+
+    if (stat(argv[1], &sb) == 0 && S_ISDIR(sb.st_mode)) {
+        playlist = 1;
+
+        int numSongs = 0;
+
+        struct dirent* file;
+
+        DIR* directory = opendir(argv[1]);
+
+        while ((file = readdir(directory)) != NULL && numSongs < 256) {
+            const char* extension = strrchr(file->d_name, '.');
+            if (extension != NULL && (strcmp(extension, ".mp3") == 0 || strcmp(extension, ".wav") == 0 || strcmp(extension, ".flac") == 0)) {
+                songs[numSongs] = (char*) malloc(256 * sizeof(char));
+                strncpy(songs[numSongs], file->d_name, 255);
+                songs[numSongs][255] = '\0';
+
+                numSongs++;
+            }
+        }
+
+        closedir(directory);
+    } else {
+        songs[0] = argv[1];
+    }
 
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         printf("Failed to initialize SDL audio system: %s\n", SDL_GetError());
@@ -28,35 +62,40 @@ int main(int argc, char* argv[]) {
         printf("Failed to open the audio device: %s\n", Mix_GetError());
         return 1;
     }
-    
-    Mix_Music* music = Mix_LoadMUS(argv[1]);
-    
-    if (!music) {
-        printf("%s", Mix_GetError());
-        return 1;
-    }
-    
-    Mix_PlayMusic(music, 1);
 
     initscr();
-    noecho();   
-
-    printw("Playing %s\n", argv[1]);
-    printw("P to pause/resume\n");
-
-    while (Mix_PlayingMusic()) {
-        if (getch() == 'p') {
-            if (Mix_PausedMusic()) {
-                Mix_ResumeMusic();
-            } else {
-                Mix_PauseMusic();
-            }
-        }
-        SDL_Delay(100);
-    }
+    nodelay(stdscr, TRUE);
+    noecho();
    
+    for(int i = 0; i < sizeof songs / sizeof songs[0]; i++) {
+        Mix_Music* music = Mix_LoadMUS(songs[i]);
+        
+        if (!music) {
+            printf("%s", Mix_GetError());
+            return 1;
+        }
+
+        Mix_PlayMusic(music, 0);
+
+        printw("Playing %s\n", songs[i]);
+        printw("P to Pause/Play\n");
+        printw("Q to Quit\n");
+
+        while (Mix_PlayingMusic()) {
+            SDL_Delay(1);
+            
+            char c = getch();
+
+            if (c == 'p')
+                Mix_PausedMusic() ? Mix_ResumeMusic() : Mix_PauseMusic();
+            else if(c == 'q')
+                return 0;
+        }
+    
+        Mix_FreeMusic(music);
+    }
+
     endwin();
-    Mix_FreeMusic(music);
     Mix_CloseAudio();
     SDL_Quit();
     
